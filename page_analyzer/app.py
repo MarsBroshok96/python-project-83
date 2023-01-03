ed9525346371f32c79908a80142c3dd0c1d2528a
MarsBroshok96 <marsbroshok1996@gmail.com>
2023-01-03 14:12:15 +0300

    Add first template of main page with bootstrap 5

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import (Flask, render_template)
 
 
 app = Flask(__name__)
 
 
-@app.get('/')
-def hello():
-    return 'Hello!'
+@app.route('/')
+def analyzer():
+    return render_template(
+        'index.html'
+    )
